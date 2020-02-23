64f08e1189fd7e6528cf465d1c40dbd1ac3ce8ef
PavelOkun <pavelokun17@gmail.com>
2020-02-23 13:01:52 +0300

    feat: add footer contents with minimum style

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import useTranslations from './useTranslations';
 
+import footerStyles from '../styles/footer.module.scss';
+
 const Footer = () => {
   const { footerText } = useTranslations();
   return (
-    <footer>
-      <p>Footer page</p>
+    <footer className={footerStyles.footer}>
+      <p>{footerText} :</p>
+      <ul>
+        <li><a href="https://github.com/muratx10" target="_blank"><img src="https://avatars0.githubusercontent.com/u/37935564?s=400&v=4" alt="muratx10"/></a></li>
+        <li><a href="https://github.com/pavelokun" target="_blank"><img src="https://avatars0.githubusercontent.com/u/53091213?s=460&v=4" alt="pavelokun"/></a></li>
+        <li><a href="https://github.com/dermeister" target="_blank"><img src="https://avatars2.githubusercontent.com/u/39797832?s=460&v=4" alt="dermeister"/></a></li>
+        <li><a href="https://github.com/yuskivpm" target="_blank"><img src="https://avatars1.githubusercontent.com/u/58133687?s=460&v=4" alt="yuskivpm"/></a></li>
+        <li><a href="https://github.com/olegmalyarenko" target="_blank"><img src="https://avatars3.githubusercontent.com/u/48599025?s=460&v=4" alt="olegmalyarenko"/></a></li>
+        <li><a href="https://github.com/ineg22" target="_blank"><img src="https://avatars3.githubusercontent.com/u/49589225?s=460&v=4" alt="ineg22"/></a></li>
+      </ul>
     </footer>
   );
 };
